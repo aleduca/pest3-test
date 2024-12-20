4e9c4b87a2c1aca0803b566b34842343dc804b54
Alexandre Cardoso <xandecar@hotmail.com>
2024-12-20 10:24:50 -0300

    feat:Create user

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/user', [UserController::class, 'create'])->name('user.create');
